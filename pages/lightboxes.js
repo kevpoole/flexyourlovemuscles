4da896d8b51468a17803e8fd9496fe3cbde3aeb2
Kevin Poole <kevitron@hotmail.com>
2022-06-03 18:47:38 -0400

    first

--- pages/lightboxes.js
+++ pages/lightboxes.js
@@ -1,8 +1,13 @@
-import styles from "../styles/Lightboxes.module.scss"
+import styles from "../styles/Lightboxes.module.scss";
 
 const Lightboxes = () => {
   return (
-    <div className={styles.body}>lightboxes</div>
-  )
-}
-export default Lightboxes
+    <div className={styles.body}>
+      <h1>Lightboxes For Sale</h1>
+      <a href="https://matthewsheltonart.wixsite.com/mysite">
+        <button className={styles.btn}>Start Shoppin'</button>
+      </a>
+    </div>
+  );
+};
+export default Lightboxes;
